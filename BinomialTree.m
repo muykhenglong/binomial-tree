cf6c0c79fffd4f14ac719e72337a63932dd9a9ad
Muykheng Long <160984366+muykhenglong@users.noreply.github.com>
2024-06-27 18:15:35 -0400

    Update BinomialTree.m

--- BinomialTree.m
+++ BinomialTree.m
@@ -1,4 +1,18 @@
 function Price = BinomialTree(S,K,T,r,u,d,p,N,IsCall,IsAmer)
+
+% function `BinomialTree` builds a pricing lattice and returns the `Price` of a European or American style call or put option.
+%  - Takes inputs:
+    % - `S`: Current stock price
+    % - `K`: Strike price of the option
+    % - `T`: Time to expiration in years
+    % - `r`: Risk-free rate
+    % - `u`: Up-factor in binomial model
+    % - `d`: Down-factor in binomial model
+    % - `p`: Probability of an up move in the binomial model
+    % - `N`: Number of time steps in the binomial model
+    % - `IsCall`: Boolean indicating if the option is a call (1) or put (0)
+    % - `IsAmer`: Boolean indicating if the option is American (1) or European (0)
+
 % Handle Call or Put
 if IsCall
     Intrinsic = @(S) max(0,S-K);
@@ -13,13 +27,13 @@ dT = T/N;
 Lattice = zeros(N+1);
 
 %Intrinsic Value at Expiration
-for i = 0:N
-    Lattice(i+1,N+1) = Intrinsic(S*u^i*d^(N-i));
+for i = 0:N % Loops through each possible state at expiration
+    Lattice(i+1,N+1) = Intrinsic(S*u^i*d^(N-i)); % Computes the intrinsic value at each node at expiration.
 end
 
-% Backwards through Tree
-for j = N:-1:1
-    for i = 1:j
+% Backwards Induction through Tree
+for j = N:-1:1 % Loops backwards through the tree from time \(N-1\) to 0 to calculate the option values
+    for i = 1:j % Loops through each node at each time step
         Lattice(i,j) = max(IsAmer * Intrinsic(S*u^(i-1)*d^(j-i)) , (p * Lattice(i+1,j+1) + (1-p) * Lattice(i,j+1)) * exp(-r*dT));
     end
 end
